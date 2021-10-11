f32e2539ef1fafc022c612241d7dc742d71f4dc8
Josh7GAS <joshuaandre@gmail.com>
2021-10-11 13:30:19 -0300

    Arcs and drawning

--- sketches/sketch-02/sketch-02.js
+++ sketches/sketch-02/sketch-02.js
@@ -1,4 +1,6 @@
 const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
     dimensions: [1080, 1080]
@@ -8,12 +10,22 @@ const degToRad = (degrees) => {
     return degrees / 100 * Math.PI;
 };
 
+const randomRange = (min, max) => {
+    return Math.random() * (max - min) + min;
+};
+
+let backgroundColor = 'white';
+let stickColor = 'black';
+let circleColor = 'red';
+
 const sketch = () => {
     return ({ context, width, height }) => {
-        context.fillStyle = 'white';
-        context.fillRect(0, 0, width, height);
+        context.fillStyle = backgroundColor;
+        context.fillRect(0, 0, width, height); //The fillRect() method
+        //draws a "filled" rectangle. The default color of the fill is black.
 
-        context.fillStyle = 'black';
+
+        context.fillStyle = stickColor;
 
         const cx = width * 0.5;
         const cy = height * 0.5;
@@ -22,37 +34,55 @@ const sketch = () => {
         const h = height * 0.1;
         let x, y;
 
-        const num = 12;
+        const num = 500;
         const radius = width * 0.3;
 
         for (let count = 0; count < num; count++) {
-            const slice = degToRad(3400 / num);
+            const slice = math.degToRad(360 / num);
             const angle = slice * count;
-
             x = cx + radius * Math.sin(angle);
             y = cy + radius * Math.cos(angle);
 
             context.save();
             context.translate(x, y);
             context.rotate(-angle);
+            context.scale(randomRange(0.1, 2), random.range(0.2, 0.5));
 
             context.beginPath();
-            context.rect(-w * 0.5, -h * 0.5, w, h);
+            context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
             context.fill();
             context.restore();
+
+            context.save();
+            context.translate(cx, cy);
+            context.rotate(-angle);
+
+            context.lineWidth = random.range(5, 20);
+
+            context.beginPath();
+            context.arc(0, 0, radius * random.range(0.7, 1.3),
+                slice * random.range(1, -8), slice * random.range(0, 5));
+            context.strokeStyle = circleColor;
+            context.stroke();
+            context.restore();
         }
-        //################## Creating a Circle##########################
-        //save state 
 
-        //restore the state
-
-        // context.translate(100, 400);
-
-        // context.beginPath();
-        // context.arc(0, 0, 50, 0, Math.PI * 2);
-        // context.fill();
-        //##############################End of the circle###############
     };
 };
 
 canvasSketch(sketch, settings);
+
+
+
+
+//################## Creating a Circle##########################
+//save state 
+
+//restore the state
+
+// context.translate(100, 400);
+
+// context.beginPath();
+// context.arc(0, 0, 50, 0, Math.PI * 2);
+// context.fill();
+//##############################End of the circle###############
