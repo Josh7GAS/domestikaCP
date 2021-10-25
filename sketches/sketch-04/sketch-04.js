7125247b7a730af31fc863d93b3d351f9f0926fa
Josh7GAS <joshuaandre@gmail.com>
2021-10-25 16:11:19 -0300

    Creating Grid WIP

---
+++ sketches/sketch-04/sketch-04.js
@@ -0,0 +1,47 @@
+const canvasSketch = require("canvas-sketch");
+
+const settings = {
+  dimensions: [1080, 1080],
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    const cols = 4;
+    const rows = 3;
+    const numCells = cols * rows;
+
+    const gridW = widht * 0.8;
+    const gridH = height * 0.8;
+    const cellW = gridW / cols;
+    const cellH = gridH / rows;
+    const margX = (width - gridW) * 0.5;
+    const margY = (height - gridH) * 0.5;
+
+    for (let count = 0; count < numCells; count++) {
+      const col = count % cols;
+      const row = Math.floor(count / cols);
+
+      const x = col * cellW;
+      const y = row * cellH;
+      const w = cellW * 0.8;
+      const h = cellH * 0.8;
+
+      context.save();
+      context.translate(x, y);
+      context.translate(margX, margY);
+      context.translate(cellW  * 0.5, cellH * 0.5);
+
+      context.beginPath();
+      context.moveTo(w * -0.5, 0);
+      context.lineTo(w * 0.5, 0);
+      context.stroke();
+
+      context.restore();
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
